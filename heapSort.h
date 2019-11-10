c2add33e3b59b19cc1a7e4783c331fa66a659b11
ernesto sanchez <Ert_Saz90@outlook.es>
2019-11-10 07:34:53 -0500

    cambio en algunas variables de la funcion heapSort.h

--- heapSort.h
+++ heapSort.h
@@ -1,44 +1,44 @@
 //Para heapificar un subárbol enraizado con el nodo i que es
 //un índice en arr []. n es el tamaño del montón
-void heapify (int *numbers, int size, int i) {
-    int largest = i;  //Inicializar más grande como root
+void heapify (int *arrNumeros, int nTam, int i) {
+    int nMayor = i;  //Inicializar más grande como root
     int l = 2*i + 1;  //izquierda = 2 *i + 1
     int r = 2*i + 2;  //right = 2 *i + 2
     int tmp;
 
     //Si el hijo izquierdo es más grande que la raíz
-    if (l < size && numbers[l] > numbers[largest]) {
-        largest = l;
+    if (l < nTam && arrNumeros[l] > arrNumeros[nMayor]) {
+        nMayor = l;
     }
 
     //Si el hijo derecho es más grande que el mayor hasta ahora
-    if (r < size && numbers[r] > numbers[largest]) {
-        largest = r;
+    if (r < nTam && arrNumeros[r] > arrNumeros[nMayor]) {
+        nMayor = r;
     }
 
     //Si el más grande no es root
-    if (largest != i) {
-        swap(i, largest, numbers);
+    if (nMayor != i) {
+        swap(i, nMayor, arrNumeros);
 
         //Heapify recursivamente el subárbol afectado
-        heapify(numbers, size, largest);
+        heapify(arrNumeros, nTam, nMayor);
     }
 }
 
 //función principal para hacer la ordenación del montón
-void heapSort (int *numbers, int size) {
+void heapSort (int *arrNumeros, int nTam) {
     int i, tmp;
     //Build heap (reorganizar matriz)
-    for (i = size / 2 - 1; i >= 0; i--) {
-        heapify(numbers, size, i);
+    for (i = nTam / 2 - 1; i >= 0; i--) {
+        heapify(arrNumeros, nTam, i);
     }
 
     //Uno por uno extrae un elemento del montón
-    for (i = size-1; i >= 0; i--) {
+    for (i = nTam-1; i >= 0; i--) {
         //Mover la raíz actual al final
-        swap(0, i, numbers);
+        swap(0, i, arrNumeros);
 
         //call max heapify en el montón reducido
-        heapify(numbers, i, 0);
+        heapify(arrNumeros, i, 0);
     }
 }
